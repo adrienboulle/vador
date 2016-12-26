5365c6ece5e0b3ff8de491a840774c0edbb1ccd6
Adrien Boullé <adrien.boulle@gadz.org>
2016-12-26 23:05:37 +0100

    clean

--- gulp/modules/copy.js
+++ gulp/modules/copy.js
@@ -47,62 +47,14 @@ const src = {
   },
 };
 
-gulp.task('copy:node_modules:prod', done =>
-  gulp.src(src.node_modules.prod, { base: '.' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:node_modules:local', done =>
-  gulp.src(src.node_modules.local, { base: '.' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:html:prod', done =>
-  gulp.src(src.html.prod, { base: 'src' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:html:local', done =>
-  gulp.src(src.html.local, { base: 'src' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:ts:prod', done =>
-  gulp.src(src.ts.prod, { base: 'app' })
-  .pipe(gulp.dest('.build/public/app'))
-  .on('end', done)
-);
-
-gulp.task('copy:ts:local', done =>
-  gulp.src(src.ts.local, { base: 'app' })
-  .pipe(gulp.dest('.build/public/app'))
-  .on('end', done)
-);
-
-gulp.task('copy:systemjs:prod', done =>
-  gulp.src(src.systemjs.prod, { base: 'src' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:systemjs:local', done =>
-  gulp.src(src.systemjs.local, { base: 'src' })
-  .pipe(gulp.dest('.build/public'))
-  .on('end', done)
-);
-
-gulp.task('copy:prod', done => {
+const copy = env => {
   const promises = [];
 
   for (let itemName in src) {
     const item = src[itemName];
     const base = item.base;
     const dest = item.dest;
-    const files = item.prod;
+    const files = item[env];
 
     if (!files || files.length === 0)
       continue;
@@ -116,31 +68,15 @@ gulp.task('copy:prod', done => {
     );
   }
 
-  Promise.all(promises)
-  .then(() => done());
-});
+  return Promise.all(promises);
+};
 
-gulp.task('copy:local', done => {
-  const promises = [];
+gulp.task('copy:local', done =>
+  copy('local')
+  .then(() => done)
+);
 
-  for (let itemName in src) {
-    const item = src[itemName];
-    const base = item.base;
-    const dest = item.dest;
-    const files = item.local;
-
-    if (!files || files.length === 0)
-      continue;
-
-    promises.push(
-      new Promise(r =>
-        gulp.src(files, { base: base })
-        .pipe(gulp.dest(dest))
-        .on('end', r)
-      )
-    );
-  }
-
-  Promise.all(promises)
-  .then(() => done());
-});
+gulp.task('copy:prod', done =>
+  copy('prod')
+  .then(() => done)
+);
