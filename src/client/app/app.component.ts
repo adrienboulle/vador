92c53b1e76d69eeffbceebc06cb0966ab1abf57e
Adrien Boullé <adrien.boulle@gadz.org>
2017-07-08 17:28:31 +0200

    add anim

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,21 +1,32 @@
-import { Component, HostBinding, HostListener } from '@angular/core';
+import { Component, HostBinding, HostListener, Renderer2, ElementRef, OnInit } from '@angular/core';
 
 declare let window: any;
+declare let document: any;
 
 @Component({
   selector: 'app',
   templateUrl: 'app.component.html',
-  host: {
-    '(window:scroll)': 'onScroll($event)',
-  }
 })
-export class AppComponent {
-  @HostBinding('class.ssr')
-  public get getClassSsr() {
-    return window.isSsr === true;
+export class AppComponent implements OnInit {
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {}
+
+  public ngOnInit(): void {
+    if (!window.isSsr) {
+      this.onScroll();
+    }
   }
 
-  public onScroll(event: Event): void {
-    console.log(event);
+  @HostBinding('class.ssr')
+  public getClassSsr(): boolean {
+    return window.isSsr;
+  }
+
+  @HostListener('window:scroll')
+  public onScroll(): void {
+    if (isNaN((window.pageYOffset || document.scrollTop)  - (document.clientTop || 0))) {
+      this._renderer.removeClass(this._elementRef.nativeElement, 'scrolled');
+    } else {
+      this._renderer.addClass(this._elementRef.nativeElement, 'scrolled');
+    }
   }
 }
