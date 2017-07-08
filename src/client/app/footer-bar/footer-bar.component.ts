f5bb7bb7863636320e60608b04202dd685685ff6
Adrien Boullé <adrien.boulle@gadz.org>
2017-07-08 17:28:41 +0200

    add anim

---
+++ src/client/app/footer-bar/footer-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-bar',
+  templateUrl: 'footer-bar.component.html',
+})
+export class FooterBarComponent {
+  constructor() {}
+}
